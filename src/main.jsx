59dfdb267f1be9c76047653d5de786f5ccd93edd
Vibhu-M27 <vibhuti22567@iiitd.ac.in>
2025-06-18 00:18:51 +0530

    fix: rename app.jsx to App.jsx to match import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import App from './App'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
